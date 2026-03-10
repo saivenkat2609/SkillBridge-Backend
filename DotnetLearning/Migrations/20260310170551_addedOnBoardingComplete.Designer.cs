2707c0512c1fac3bb76907606b8d2e2ca695489c
sai venkat <sai.rajam@keyloop.com>
2026-03-10 22:36:18 +0530

    updated migrations for isOnBoardignComplete column

--- DotnetLearning/Migrations/20260310170551_addedOnBoardingComplete.Designer.cs
+++ DotnetLearning/Migrations/20260310170551_addedOnBoardingComplete.Designer.cs
@@ -4,6 +4,7 @@ using DotnetLearning.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotnetLearning.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260310170551_addedOnBoardingComplete")]
+    partial class addedOnBoardingComplete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DotnetLearning/Migrations/20260310170551_addedOnBoardingComplete.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotnetLearning.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedOnBoardingComplete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOnboardingComplete",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOnboardingComplete",
+                table: "AspNetUsers");
+        }
+    }
+}
